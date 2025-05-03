b76d939803701d03a8010bf5c52d286e81e60773
matisse5578 <shoppingaube@gmail.com>
2025-05-03 19:35:37 +0200

    Update and rename quequette flix javascrpt.js to videos.json

--- quequette flix javascrpt.js
+++
@@ -1,8 +0,0 @@
-function lancerVideo() {
-  const video = document.getElementById('maVideo');
-  video.src = "https://www.w3schools.com/html/mov_bbb.mp4"; // exemple de vidéo propre
-  video.style.display = 'block';
-  video.play();
-}
-
-
